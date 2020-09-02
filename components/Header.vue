05dfc0c87475909d07f156b03cd3dfe6477191dc
Yunus Altın <yunus_altin_1963@gmail.com>
2020-09-02 16:42:07 +0300

    Update Header.vue

--- components/Header.vue
+++ components/Header.vue
@@ -1,32 +1,8 @@
-<template>
-  <div class="header">
-    <div class="logo">
-      <h2 class="logo__title">easysnap</h2>
-    </div>
-    <div class="header_menu">
-      <nuxt-link to="/" tag="a" exact>snaps</nuxt-link>
-      <span v-if="initLocal || activeUser">
-        <nuxt-link to="/profile" tag="a">@{{activeUser.username}}</nuxt-link>
-        <Logout />
-      </span>
-      <span v-else>
-        <nuxt-link to="/login" tag="a">login</nuxt-link>
-        <nuxt-link to="/join" tag="a">join</nuxt-link>
-      </span>
-    </div>
-  </div>
-</template>
-
 <script>
 import Logout from './Logout';
 import { GET_ACTIVE_USER } from '~/queries';
 
 export default {
-  methods: {
-    initLocal() {
-      return localStorage.getItem('apollo-token') != null
-    }
-  },
   apollo: {
     activeUser: {
       query: GET_ACTIVE_USER
@@ -37,3 +13,22 @@ export default {
   }
 };
 </script>
+
+<template>
+  <div class="header">
+    <div class="logo">
+      <h2 class="logo__title">easysnap</h2>
+    </div>
+    <div class="header_menu">
+      <nuxt-link to="/" tag="a" exact>snaps</nuxt-link>
+      <span v-if="activeUser">
+        <nuxt-link to="/profile" tag="a">@{{activeUser.username}}</nuxt-link>
+        <Logout />
+      </span>
+      <span v-else>
+        <nuxt-link to="/login" tag="a">login</nuxt-link>
+        <nuxt-link to="/join" tag="a">join</nuxt-link>
+      </span>
+    </div>
+  </div>
+</template>
