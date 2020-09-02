17148459a44d81d4b0c228559bb3e4c82a4a691e
Yunus Altın <yunus_altin_1963@gmail.com>
2020-09-02 14:10:50 +0300

    Update Header.vue

--- components/Header.vue
+++ components/Header.vue
@@ -5,8 +5,8 @@
     </div>
     <div class="header_menu">
       <nuxt-link to="/" tag="a" exact>snaps</nuxt-link>
-      <nuxt-link to="/profile" tag="a" v-if="$store.state.activeUser">@{{$store.state.activeUser.username}}</nuxt-link>
-      <Logout v-if="$store.state.activeUser" />
+      <nuxt-link to="/profile" tag="a" v-if="$apolloHelpers.getToken()">@{{$store.state.activeUser.username}}</nuxt-link>
+      <Logout v-if="$apolloHelpers.getToken()" />
       <div v-else>
         <nuxt-link to="/login" tag="a">login</nuxt-link>
         <nuxt-link to="/join" tag="a">join</nuxt-link>
