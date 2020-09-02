2a6b2ed01621f033f998cce8afe79f4e062eb77b
Yunus Altın <yunus_altin_1963@gmail.com>
2020-09-02 15:07:43 +0300

    Update Header.vue

--- components/Header.vue
+++ components/Header.vue
@@ -6,7 +6,7 @@
     <div class="header_menu">
       <nuxt-link to="/" tag="a" exact>snaps</nuxt-link>
       <nuxt-link to="/profile" tag="a" v-if="activeUser">@{{activeUser.username}}</nuxt-link>
-      <Logout v-if="activeUser" />
+      <Logout v-if="!activeUser" />
       <div v-else>
         <nuxt-link to="/login" tag="a">login</nuxt-link>
         <nuxt-link to="/join" tag="a">join</nuxt-link>
