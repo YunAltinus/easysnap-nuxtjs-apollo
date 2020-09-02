9d03bbdc64652dd4609b2e98dda334cf5d7fb2c3
Yunus Altın <yunus_altin_1963@gmail.com>
2020-09-02 19:21:21 +0300

    Update index.js

--- store/index.js
+++ store/index.js
@@ -17,14 +17,17 @@ const createStore = () => {
     },
     actions: {
       nuxtServerInit({ commit }, { req }) {
-        // if (req.headers && req.headers !== null) {
-        //   try {
-        //     const checkToken = req.headers.cookie.split('=')[1];
-        //     commit('setToken', checkToken);
-        //   } catch (error) {
-        //     console.log(error);
-        //   }
-        // }
+        if (req) {
+          const token = req.headers;
+          if (token && token !== null) {
+            try {
+              const checkToken = token.cookie.split('=')[1];
+              commit('setToken', checkToken);
+            } catch (error) {
+              console.log(error);
+            }
+          }
+        }
       },
       singIn({ commit, dispatch }, token) {
         this.$apolloHelpers.onLogin(token);
